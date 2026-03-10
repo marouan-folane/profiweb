9421950777b40daa7cee0eb8859ff8ab84652cfb
marouan-folane <marouanfolane@gmail.com>
2026-03-10 09:07:01 +0000

    fv

--- api/services/notificationService.js
+++ api/services/notificationService.js
@@ -1,4 +1,4 @@
-const Notification = require('../models/notification.model');
+ const Notification = require('../models/notification.model');
 const User = require('../models/user.model');
 const Role = require('../models/role.model');
 
